b57857b1162f3b7d396dbe5da16f676f6a8d0ccc
Chris Bacani <chrisbacani@Chriss-MacBook.local>
2018-03-15 15:23:22 -0400

    Homework 3- Psychic Game

--- Psychic-Game/javascript/psychic.js
+++ Psychic-Game/javascript/psychic.js
@@ -1,3 +1,4 @@
+
 //Define what letters the computer can use as a guess
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -14,48 +15,46 @@
     console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + guessesRemaining
     + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 // Listening for User Input
-document.onkeyup = function(event) {
+	
+	
 
-	// When user presses a key, it records it and saves to userGuess
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		document.onkeyup = function(event) {
 
-	if (guessesSoFar.indexOf(userGuess) < 0 && alphabet.indexOf(userGuess) >= 0) {
+			// When user presses a key, it records it and saves to userGuess
+			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		
+			if (letterToBeGuessed == userGuess) {
+				wins++;
+				alert("You won!");
+				guessesRemaining = 10;
+				guessesSoFar = [];
+				letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
+				console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + 
+				guessesRemaining + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+				
+				document.getElementById("wins").textContent = wins;
+			}
+
+	else if (guessesSoFar.indexOf(userGuess) < 0 && alphabet.indexOf(userGuess) >= 0) {
 		// guessesSoFar(guessesSoFar.length)=userGuess;
 		// if it is a new letter then decrease remaining guesses by 1
 		guessesRemaining--;
+		
+		guessesSoFar.push(userGuess);
+		if (guessesRemaining === 0) {
+			losses++;
+			alert("You lost!");
+			guessesRemaining = 10;
+			guessesSoFar = [];
+			letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
+			console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + guessesRemaining + 
+			" Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+			
+			document.getElementById("losses").textContent = losses;}
 
 		document.getElementById("guessesRemaining").textContent = guessesRemaining;
+
+		document.getElementById("guessesSoFar").textContent = guessesSoFar;
 	}
-	// if letterToBeGuessed is same as userGuess then it will record as a win, guesses resets to 10, computer picks new value
-	if (letterToBeGuessed == userGuess) {
-		wins++;
-		alert("You won!");
-		guessesRemaining = 10;
-		guessesSoFar = [];
-		letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + 
-		guessesRemaining + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
-		
-		document.getElementById("wins").textContent = wins;
-	}
-
-	// if guessesRemaining gets to 0 then record it as a loss, guessesRemaining will reset to 0, and computer picks new value
-	if (guessesRemaining === 0) {
-		losses++;
-		console.log("You lost!");
-		guessesRemaining = 10;
-		guessesSoFar = [];
-		letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + guessesRemaining + 
-		" Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
-		
-		document.getElementById("losses").textContent = losses;
-	}
-
-
-
-	// Displaying progress to HTML
-    // var html = "<h1>The Psychic Game</h1>" +  "<h2>Guess which letter I'm thinking of</h2>" + "<h2>Wins: 0" + wins + "</h2>" + "<h2>Losses: 0" + losses + "</h2>" + "<h2>Guesses Remaining: 10" + guessesRemaining + "</h2>" + "<p><h2>Guesses so far: " + guessesSoFar + "</h2></p>";
+		}
 	
-	// document.querySelector("#game").innerHTML = html;
-}
