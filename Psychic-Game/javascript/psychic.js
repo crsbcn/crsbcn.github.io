0c6e40db88f184af5266839073066d02ae45ed4f
Chris Bacani <chrisbacani@Chriss-MacBook.local>
2018-03-15 13:09:53 -0400

    further JS changes

--- Psychic-Game/javascript/psychic.js
+++ Psychic-Game/javascript/psychic.js
@@ -1,5 +1,5 @@
 //Define what letters the computer can use as a guess
-    var Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //Set initial global variables///////////////////////////////////////////////////////////
     var wins = 0
@@ -13,18 +13,18 @@
     var letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + guessesRemaining
     + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
-////////////////////////////////////////////
 // Listening for User Input
-// start listening for events
 document.onkeyup = function(event) {
 
 	// When user presses a key, it records it and saves to userGuess
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
-		guessesSoFar[guessesSoFar.length]=userGuess;
+	if (guessesSoFar.indexOf(userGuess) < 0 && alphabet.indexOf(userGuess) >= 0) {
+		// guessesSoFar(guessesSoFar.length)=userGuess;
 		// if it is a new letter then decrease remaining guesses by 1
 		guessesRemaining--;
+
+		document.getElementById("guessesRemaining").textContent = guessesRemaining;
 	}
 	// if letterToBeGuessed is same as userGuess then it will record as a win, guesses resets to 10, computer picks new value
 	if (letterToBeGuessed == userGuess) {
@@ -34,7 +34,9 @@ document.onkeyup = function(event) {
 		guessesSoFar = [];
 		letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
         console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + 
-        guessesRemaining + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+		guessesRemaining + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+		
+		document.getElementById("wins").textContent = wins;
 	}
 
 	// if guessesRemaining gets to 0 then record it as a loss, guessesRemaining will reset to 0, and computer picks new value
@@ -45,10 +47,15 @@ document.onkeyup = function(event) {
 		guessesSoFar = [];
 		letterToBeGuessed = alphabet[Math.floor(Math.random() * alphabet.length)];
         console.log("Wins: " + wins + " Losses: " + losses + " Guesses Remaining: " + guessesRemaining + 
-        " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+		" Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+		
+		document.getElementById("losses").textContent = losses;
 	}
 
+
+
 	// Displaying progress to HTML
-    var html = "<h1>The Psychic Game</h1>" +  "<h2>Guess which letter I'm thinking of</h2>" + "<h2>Wins: 0" + wins + "</h2>" + "<h2>Losses: 0" + losses + "</h2>" + "<h2>Guesses Remaining: 10" + guessesRemaining + "</h2>" + "<p><h2>Guesses so far: " + guessesSoFar + "</h2></p>";
-	// place html into the game ID
-	document.querySelector("#game").innerHTML = html;}
+    // var html = "<h1>The Psychic Game</h1>" +  "<h2>Guess which letter I'm thinking of</h2>" + "<h2>Wins: 0" + wins + "</h2>" + "<h2>Losses: 0" + losses + "</h2>" + "<h2>Guesses Remaining: 10" + guessesRemaining + "</h2>" + "<p><h2>Guesses so far: " + guessesSoFar + "</h2></p>";
+	
+	// document.querySelector("#game").innerHTML = html;
+}
